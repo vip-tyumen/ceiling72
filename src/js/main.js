8defe2c432b1c75b3b6ba4352800366479a88aba
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2022-08-11 18:38:25 +0400

    update

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+;(function($) {
+	/**
+	 * --NAVIGATION
+	 **/
+	$('.nav-btn').on('click', function(e) {
+		e.preventDefault();
+		let parent = $(this).closest('.navigation'),
+			menu = $(".navigation--wrapper", parent),
+			nav = $("nav", menu),
+			height = nav.outerHeight(true) + `px`;
+		if(!$(parent).hasClass('open')) {
+			menu.css({
+				height: height
+			});
+		} else {
+			menu.css({
+				height: 0
+			});
+		}
+		$(parent).toggleClass('open');
+		return !1;
+	});
+	$(window).on('resize', function(){
+		let parent = $('.navigation'),
+			menu = $(".navigation--wrapper", parent),
+			nav = $("nav", menu),
+			height = nav.outerHeight(true) + `px`;
+		if($(parent).hasClass('open')) {
+			if($(document).width() <= 590) {
+				menu.css({
+					height: height
+				});
+			} else {
+				menu.removeAttr('style')
+			}
+		} else {
+			if($(document).width() <= 590) {
+				menu.css({
+					height: 0
+				});
+			} else {
+				menu.removeAttr('style')
+			}
+		}
+	}).trigger('resize');
+	/**
+	 * /-NAVIGATION
+	 **/
+})(jQuery);
